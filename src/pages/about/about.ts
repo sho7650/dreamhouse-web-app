0ce1d4df5d2bc4b399a354d376f80632f5fcba83
sho kisaragi <sho@oshiire.to>
2019-08-07 16:02:48 +0900

    fix loading async_load

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
-declare var async_load;
+declare var window.attachEvent;
 
 @Component({
   selector: 'page-about',
@@ -13,9 +13,16 @@ export class AboutPage {
   constructor(public navCtrl: NavController) {
     console.log("about.html");
 
-    //if (Window.attachEvent) { window.attachEvent('onload', async_load); }
-    // else { window.addEventListener('load', async_load, false); }
-    window.addEventListener('load', async_load, false);
+    (function () {
+      console.log("running Pardot tag");
+      function async_load() {
+        var s = document.createElement('script'); s.type = 'text/javascript';
+        s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+        var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+      }
+      if (window.attachEvent) { window.attachEvent('onload', async_load); }
+      else { window.addEventListener('load', async_load, false); }
+    })();
   }
 
 }
