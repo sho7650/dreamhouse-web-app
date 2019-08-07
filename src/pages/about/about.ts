b59ba766687aa1efa3561869b28e3f64cd5aedcc
sho kisaragi <sho@oshiire.to>
2019-08-07 15:44:13 +0900

    update pardot tracking function

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-    selector: 'page-about',
-    templateUrl: 'about.html'
+  selector: 'page-about',
+  templateUrl: 'about.html'
 })
 export class AboutPage {
 
   constructor(public navCtrl: NavController) {
+    console.log("about.html");
+
+    if (window.attachEvent) { window.attachEvent('onload', async_load); }
+    else { window.addEventListener('load', async_load, false); }
 
   }
 
