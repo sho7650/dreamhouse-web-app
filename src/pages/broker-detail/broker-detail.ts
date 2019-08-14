97a2328d493aa56bf89eab61fc5a797916fd5177
sho kisaragi <sho@oshiire.to>
2019-08-14 16:04:59 +0900

    debuging ...

--- src/pages/broker-detail/broker-detail.ts
+++ src/pages/broker-detail/broker-detail.ts
@@ -9,15 +9,18 @@ declare var interval_id, findTitle, piTracker;
     templateUrl: 'broker-detail.html'
 })
 export class BrokerDetailPage {
-
+    title: string;
     broker: any;
 
     constructor(public navCtrl: NavController, public navParams: NavParams, public service: BrokerService) {
+        this.title = document.title;
+        console.log(this.title);
         this.broker = this.navParams.data;
-        piTracker(document.URL + "broker-detail/" + this.broker.id);
 
         service.findById(this.broker.id).then(
             broker => this.broker = broker
+        ).then(
+            () => piTracker(document.URL + "broker-detail/" + this.broker.id)
         );
         // interval_id = setInterval(findTitle(document.URL + "broker-detail/" + this.broker.id, '^担当者'), 100);
         // setTimeout(piTracker(document.URL + "broker-detail/" + this.broker.id), 1000);
